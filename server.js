b01ffda5555dfc2c5bc8c8ccc7865e46fe6f64bc
Jasna Jaison <jasnajaison7@gmail.com>
2025-06-30 19:52:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS
       "http://127.0.0.1:8080",
       "https://glowing-duckanoo-1193ca.netlify.app",
       /\.netlify\.app$/,
-     /\.onrender\.com$/
+     /\.onrender\.com$/,
+    /\.up\.railway\.app$/
     ];
 
 const io = new Server(PORT, {
